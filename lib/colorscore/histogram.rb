7e5a833933a4d155210446391acfc0a695da90fc
Andrea Dal Ponte <dalpo85@gmail.com>
2012-09-08 16:07:30 +0200

    restrict regexp to six values

--- lib/colorscore/histogram.rb
+++ lib/colorscore/histogram.rb
@@ -7,7 +7,7 @@ module Colorscore
 
     # Returns an array of colors in descending order of occurances.
     def colors
-      hex_values = @lines.map { |line| line[/#[0-9A-F]+/] }
+      hex_values = @lines.map { |line| line[/#[0-9A-F]{6}+/] }
       hex_values.map { |hex| Color::RGB.from_html(*hex) }
     end
 
